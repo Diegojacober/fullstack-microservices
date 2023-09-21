9e927e7c761f221dee6ce9e6b2af286c503a62ac
ETS <ct67ca@bosch.com>
2023-09-21 11:10:35 -0300

    Django views

--- django/admin/products/views.py
+++ django/admin/products/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from .models import Product, User
+from .serializers import ProductSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+import random
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request): #/api/products
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+        
+    def create(self, request): #/api/products
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+    def retrieve(self, request, pk=None): #/api/products/<str:id>
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(product) 
+        return Response(serializer.data)
+        
+    def update(self, request,  pk=None): #/api/products/<str:id>
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(instance=product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save() 
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        
+    def destroy(self, request,  pk=None): #/api/products/<str:id>
+        product = Product.objects.get(id=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class UserAPIView(APIView):
+    def get(self, _):
+        users = User.objects.all()
+        user = random.choice(users)
+        return Response({
+            'id': user.id
+        })
